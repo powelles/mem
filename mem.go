dd08dbf4869b7f9b9eee85389f69b7327d173ff0
Justin Powell <rodeojones78@gmail.com>
2016-02-26 16:26:49 -0800

    Create mem.go

---
+++ mem.go
@@ -0,0 +1,38 @@
+// Package mem easily gets current memory usage
+package mem
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Get returns a string of current memory usage such as "8KB" or "16M"
+func Get() string {
+	s := new(runtime.MemStats)
+	runtime.ReadMemStats(s)
+	m := s.TotalAlloc
+
+	var i int
+	for {
+		if m > 1024 {
+			m = m / 1024
+		} else {
+			break
+		}
+		i++
+	}
+
+	b := make(map[int]string, 6)
+	b[0] = "B"
+	b[1] = "KB"
+	b[2] = "MB"
+	b[3] = "GB"
+	b[4] = "TB"
+	b[5] = "PB"
+
+	if i > 5 {
+		panic("Mem: We don't deal in anything larger than Petabytes")
+	}
+
+	return fmt.Sprintf("%d"+b[i], m)
+}
